7b50006b1b1af3bca7cbcdabd19d0925fa7f8510
jnuzzi4 <jnuzzi47@gmail.com>
2020-03-24 16:53:10 -0500

    deleted the num-of-cards integer

--- db/migrate/20200324200007_create_decks.rb
+++ db/migrate/20200324200007_create_decks.rb
@@ -2,8 +2,8 @@ class CreateDecks < ActiveRecord::Migration[5.0]
   def change
     create_table :decks do |t|
       t.string  :title
-      t.integer :num_of_cards
       t.string  :rank
     end
   end
 end
+  
