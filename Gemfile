9bfab2cb7f47e511fee15c7e289cee018cc333f9
Tr1ckC0 <trick.co@yahoo.com>
2020-03-24 20:58:13 -0500

    basic model test data

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 source "https://rubygems.org"
 
+gem 'faker'
 gem "rest-client"
 gem "json"
 gem "sinatra-activerecord"
--- bin/run.rb
+++ bin/run.rb
@@ -1,4 +1,6 @@
 require_relative '../config/environment'
 
 ##### TESTS #######
-# 10.times do {|i| Card.new(id: i, name: "#")
+10.times do {|i| Card.new(id: i, name: "#{Faker::Name.name}"}
+10.times do {|i| User.new(id: i, name: "#{Faker::Name.name}"}
+10.times do {|i| Deck.new(id: i, username: "#{Faker::Name.name}")}
