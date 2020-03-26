b943a4df0b7ecc7eae89a604d59477ad01575fe7
Tr1ckC0 <trick.co@yahoo.com>
2020-03-25 19:27:23 -0500

    added Card#display

--- lib/models/card.rb
+++ lib/models/card.rb
@@ -3,4 +3,27 @@ class Card < ActiveRecord::Base
     has_many :user_cards
     has_many :decks, through: :deck_cards
     has_many :users, through: :user_cards
+
+
+    def display
+        puts "------------------------------------------------------------------------------------------------------------"
+        puts ""
+        puts "NAME              -    #{self.name}"
+        puts "MANA COST         -    #{self.mana_cost}"
+        puts "TOTAL MANA COST   -    #{self.total_mana_cost}"
+        puts "COLOR             -    #{self.color}"
+        puts "MANA TYPES        -    #{self.mana_type}"
+        puts "TYPES             -    #{self.types}"
+        puts "SUBTYPES          -    #{self.subtypes}"
+        puts "RARITY            -    #{self.rarity}"
+        puts "SET               -    #{self.set}"
+        puts "POWER             -    #{self.power}"
+        puts "TOUGHNESS         -    #{self.toughness}"
+        puts "************************************************************************************************************"
+        puts self.text
+        puts "************************************************************************************************************"
+        puts ""
+        puts "LEGALITY          -    #{self.legality}"
+        puts "VIEW IMAGE        -    #{self.imageURL}"
+    end
 end
